59941c0fd77510df5fd815af5fca6b6117596381
Aravind <aravindhraj071003@gmail.com>
2023-07-15 17:49:52 +0530

    Added useEffect from Hooks

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import './App.css';
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 
+  // ------------ USE EFFECT ------------ 
+  useEffect(() => {
+    setItems(JSON.parse(localStorage.getItem('to-do-list')))
+    console.log("Rendering")
+  }, [])
+
+
   // ------------ USE STATE ------------ 
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('to-do-list')) || [])
+  const [items, setItems] = useState([])
 
   const [addItem, setAddItem] = useState('')
   const [searchItem, setSearchItem] = useState('')
 
-  // ------------ HANDLER FUNCTION ------------ 
+  // ----------------------- HANDLER FUNCTION ----------------------- 
+
+  // ----------- WHEN CHANGE ON CHECKBOX THIS EVENT HANDLE -----------
 
   const handleChange = (id) => {
     const listener = items.map(item => {
@@ -22,6 +31,8 @@ function App() {
     localStorage.setItem('to-do-list', JSON.stringify(listener))
   }
 
+  // ----------- WHEN DELETE THE ITEM THIS EVENT HANDLES -----------
+
   const handleDelete = (id) => {
     const listener = items.filter(item => {
       return item.id !== id
@@ -29,6 +40,8 @@ function App() {
     setItems(listener)
     localStorage.setItem('to-do-list', JSON.stringify(listener))
   }
+
+  // ----------- WHEN ADD NEW ITEM IN THE ITEMS THIS EVENT HANDLES -----------
 
   const handleAppEvent = (e) => {
     e.preventDefault()
